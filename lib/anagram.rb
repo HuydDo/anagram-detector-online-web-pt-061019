616bc29fbcf4e788d74a5624a4d2dfe2e2272dbc
Huy Do <huyddo40@gmail.com>
2019-06-19 06:13:59 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+  def match(array)
+    @word.split("")
+    binding.pry
+    array.detect{|i| @word}
+  end
+end
